64056771be17b5b7638fc901be6380eb70aa2e50
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-07-13 12:14:24 +0300

    refactor pages exports

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,5 @@
-export const NotFound = () => {
+const NotFound = () => {
   return <div>404</div>
 }
+
+export default NotFound
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,3 +1,5 @@
 const ServerError = () => {
   return <div>500 Server Error</div>
 }
+
+export default ServerError
